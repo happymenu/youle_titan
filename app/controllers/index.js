3b1cb143a0b513e07b610f2a52cf00541d0a1f6f
guo weidong <guowind@live.com>
2014-11-13 15:13:52 +0800

    有了 Titanium,实现了登陆功能和主界面展示，第一次提交

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,12 @@
 function doClick(e) {
     alert($.label.text);
 }
-
+function do_login(e){
+	var main_win = Alloy.createController('main').getView();
+	main_win.open();
+}
 $.index.open();
+
+
 
 
