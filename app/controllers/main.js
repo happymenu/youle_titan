1426b9d7c218abe3aea4cbb1e15881ce001b1622
guo weidong <guowind@live.com>
2014-11-13 21:44:09 +0800

    Version 2 在原有版本的基础上，完成了主界面各个按钮的点击效果，同时搞定了印象功能的界面展示，学到了很多（scrollView,zIndex...)

---
+++ app/controllers/main.js
@@ -0,0 +1,21 @@
+function house(e){
+	var house_win = Alloy.createController('house').getView();
+	house_win.open();
+}
+function camera(e){
+	var camera_win = Alloy.createController('camera').getView();
+	camera_win.open();
+}
+function check(e){
+	var check_win = Alloy.createController('check').getView();
+	check_win.open();
+}
+function search(e){
+	var search_win = Alloy.createController('search').getView();
+	search_win.open();
+}
+function shopping(e){
+	var shopping_win = Alloy.createController('shopping').getView();
+	shopping_win.open();
+}
+$.main.open();
